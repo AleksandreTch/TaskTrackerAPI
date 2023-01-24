0c63e799934dde3e1119fe4d7597b21166043ff1
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2023-01-23 23:00:33 -0500

    Adding the custom hook to App.js

--- src/App.js
+++ src/App.js
@@ -1,55 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-ace5c-default-rtdb.firebaseio.com/tasks.json'
-      );
+  const transformTasks = (taskObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ 
-          id: taskKey, 
-          text: data[taskKey].text 
-        });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in taskObj) {
+      loadedTasks.push({
+        id: taskKey,
+        text: taskObj[taskKey].text,
+      });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp({
+    url: "https://react-http-ace5c-default-rtdb.firebaseio.com/tasks.json", transformTasks
+  });
 
   useEffect(() => {
     fetchTasks();
-  }, []);
+  }, [fetchTasks]);
 
   const taskAddHandler = async (task) => {
-    const response = await fetch('https://taskapp-c158b-default-rtdb.firebaseio.com/tasks.json', {
-        method: 'POST',
+    const response = await fetch(
+      "https://taskapp-c158b-default-rtdb.firebaseio.com/tasks.json",
+      {
+        method: "POST",
         body: JSON.stringify(task),
         headers: {
-          'Content-Type' : 'application/json'
-        }
-    });
+          "Content-Type": "application/json",
+        },
+      }
+    );
     setTasks((prevTasks) => prevTasks.concat(task));
   };
 
